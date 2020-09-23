b9d429020632bd820b83b1070d77a2d49244d11c
Valentin <valentin-didomenico@orange.fr>
2020-09-23 10:58:32 +0200

    [ADD] AudioManager + [UPDATE] gitignore

---
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AK.Wwise.Event PlayMainMusic;
+    public AK.Wwise.Event PlayPositiveFB;
+    public AK.Wwise.Event PlayNegativeFB;
+    public AK.Wwise.Event PlayStartCityPlugged;
+    public AK.Wwise.Event PlayEndCityPlugged;
+    public AK.Wwise.Event PlayVocalLog;
+
+    public static AudioManager instance;
+
+    private void Awake()
+    {
+        if (instance)
+            Destroy(gameObject);
+        else
+            instance = this;
+    }
+}
