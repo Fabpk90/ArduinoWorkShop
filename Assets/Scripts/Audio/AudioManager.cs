9684ead66f4668b614b3e388bc85cb9a55c801b1
Valentin <valentin-didomenico@orange.fr>
2020-09-25 10:11:13 +0200

    [UPDATE] SNB and AudioManager

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -4,12 +4,18 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
-    public AK.Wwise.Event PlayMainMusic;
-    public AK.Wwise.Event PlayPositiveFB;
-    public AK.Wwise.Event PlayNegativeFB;
-    public AK.Wwise.Event PlayStartCityPlugged;
-    public AK.Wwise.Event PlayEndCityPlugged;
-    public AK.Wwise.Event PlayVocalLog;
+    public AK.Wwise.Event Music_Main;
+    public AK.Wwise.Event Feedback_Apparition;
+    public AK.Wwise.Event Feedback_FinalCity;
+    public AK.Wwise.Event Feedback_Negative;
+    public AK.Wwise.Event Feedback_Neutre;
+    public AK.Wwise.Event Feedback_Positive;
+    public AK.Wwise.Event Feedback_Start;
+    public AK.Wwise.Event Tutorial_CityDestroyeds;
+
+    public AK.Wwise.Event[] tutorialLogs;
+
+    private int tutoStepCounter;
 
     public static AudioManager instance;
 
@@ -19,5 +25,17 @@ public class AudioManager : MonoBehaviour
             Destroy(gameObject);
         else
             instance = this;
+
+        tutoStepCounter = 0;
+    }
+
+    public void IncLogCounter()
+    {
+        tutoStepCounter++;
+    }
+
+    public void TriggerVoice()
+    {
+        tutorialLogs[tutoStepCounter].Post(gameObject);
     }
 }
